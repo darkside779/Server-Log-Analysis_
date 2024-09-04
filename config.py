f1784e0afd62b07b54495bc80a98358fa934eb5c
Darkside <86417207+darkside779@users.noreply.github.com>
2024-09-04 05:31:58 +0200

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')  # Use SQLite for simplicity
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
